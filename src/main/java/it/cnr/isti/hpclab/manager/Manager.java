b3aab1c327147e764cf09e23e690cf7bb22d50bd
Craig Macdonald <craig.macdonald_AT_glasgow.ac.uk>
2019-04-29 23:18:05 +0100

    bootstrap micro-Manager to implement Terrier-Core's Matching interface #3

--- src/main/java/it/cnr/isti/hpclab/manager/Manager.java
+++ src/main/java/it/cnr/isti/hpclab/manager/Manager.java
@@ -20,80 +20,82 @@
 
 package it.cnr.isti.hpclab.manager;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.io.Closeable;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
+import org.terrier.matching.Matching;
+import org.terrier.matching.MatchingQueryTerms;
+import org.terrier.matching.MatchingQueryTerms.MatchingTerm;
+import org.terrier.matching.QueryResultSet;
+import org.terrier.matching.matchops.SingleTermOp;
+import org.terrier.structures.CollectionStatistics;
 import org.terrier.structures.Index;
 
 import it.cnr.isti.hpclab.MatchingConfiguration;
 import it.cnr.isti.hpclab.MatchingConfiguration.Property;
 import it.cnr.isti.hpclab.matching.MatchingAlgorithm;
+import it.cnr.isti.hpclab.matching.structures.Query;
 import it.cnr.isti.hpclab.matching.structures.ResultSet;
 import it.cnr.isti.hpclab.matching.structures.SearchRequest;
 import it.cnr.isti.hpclab.matching.structures.WeightingModel;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 /**
- * This class implements the component actually driving the low-level query processing activities.
- * It basically manages the objects and configurations shared by all queries in a batch experiments,
- * and invokes the query processing algorithm.
+ * This class implements the component actually driving the low-level query
+ * processing activities. It basically manages the objects and configurations
+ * shared by all queries in a batch experiments, and invokes the query
+ * processing algorithm.
  */
-public abstract class Manager implements Closeable
-{
+public abstract class Manager implements Closeable, Matching {
 	protected static final Logger LOGGER = Logger.getLogger(Manager.class);
 	protected static boolean IGNORE_LOW_IDF_TERMS = MatchingConfiguration.getBoolean(Property.IGNORE_LOW_IDF_TERMS);
-	
+
 	protected Index mIndex = null;
 	public WeightingModel mWeightingModel = null;
 	protected MatchingAlgorithm mMatchingAlgorithm = null;
 
 	public long processingTime;
 	public long processedPostings;
-	
+
 	public long partiallyProcessedDocuments;
 	public long numPivots;
 
-	public Manager()
-	{
+	public Manager() {
 		this(null);
 	}
 
-	public Manager(final Index index)
-	{
+	public Manager(final Index index) {
 		String weightingModelClassName = MatchingConfiguration.get(Property.WEIGHTING_MODEL_CLASSNAME);
 		createWeigthingModel(weightingModelClassName);
-		
-		String matchingAlgorithmClassName =  MatchingConfiguration.get(Property.MATCHING_ALGORITHM_CLASSNAME); 
+
+		String matchingAlgorithmClassName = MatchingConfiguration.get(Property.MATCHING_ALGORITHM_CLASSNAME);
 		createMatchingAlgorithm(matchingAlgorithmClassName);
-		
+
 		loadIndex(index);
 	}
 
-	public Manager(final Index index, final String matchingAlgorithmClassName)
-	{
+	public Manager(final Index index, final String matchingAlgorithmClassName) {
 		String weightingModelClassName = MatchingConfiguration.get(Property.WEIGHTING_MODEL_CLASSNAME);
 		createWeigthingModel(weightingModelClassName);
-		 
+
 		createMatchingAlgorithm(matchingAlgorithmClassName);
-		
+
 		loadIndex(index);
 	}
 
-	public Manager(final Index index, final String weightingModelClassName, final String matchingAlgorithmClassName)
-	{
-		createWeigthingModel(weightingModelClassName);	
+	public Manager(final Index index, final String weightingModelClassName, final String matchingAlgorithmClassName) {
+		createWeigthingModel(weightingModelClassName);
 		createMatchingAlgorithm(matchingAlgorithmClassName);
-		
+
 		loadIndex(index);
 	}
 
-	protected void loadIndex(final Index index) 
-	{
+	protected void loadIndex(final Index index) {
 		if (index == null)
 			mIndex = Index.createIndex();
-		else 
+		else
 			mIndex = index;
 		if (mIndex == null) {
 			LOGGER.fatal("Failed to load index. " + Index.getLastIndexLoadError());
@@ -104,44 +106,105 @@ public abstract class Manager implements Closeable
 		mMatchingAlgorithm.setup(this);
 	}
 
-	protected void createWeigthingModel(final String weightingModelClassName) 
-	{
+	protected void createWeigthingModel(final String weightingModelClassName) {
 		checkNotNull(weightingModelClassName);
 		try {
 			if (weightingModelClassName.indexOf('.') == -1)
-				mWeightingModel = (WeightingModel) (Class.forName(MatchingConfiguration.get(Property.DEFAULT_NAMESPACE) + "structures.model." + weightingModelClassName).asSubclass(WeightingModel.class).getConstructor().newInstance());
+				mWeightingModel = (WeightingModel) (Class
+						.forName(MatchingConfiguration.get(Property.DEFAULT_NAMESPACE) + "structures.model."
+								+ weightingModelClassName)
+						.asSubclass(WeightingModel.class).getConstructor().newInstance());
 			else
-				mWeightingModel = (WeightingModel) (Class.forName(weightingModelClassName).asSubclass(WeightingModel.class).getConstructor().newInstance());
+				mWeightingModel = (WeightingModel) (Class.forName(weightingModelClassName)
+						.asSubclass(WeightingModel.class).getConstructor().newInstance());
 		} catch (Exception e) {
 			LOGGER.error("Problem loading weighting model (" + weightingModelClassName + "): ", e);
 		}
 	}
 
-	protected void createMatchingAlgorithm(final String matchingAlgorithmClassName) 
-	{
+	protected void createMatchingAlgorithm(final String matchingAlgorithmClassName) {
 		checkNotNull(matchingAlgorithmClassName);
 		try {
 			if (matchingAlgorithmClassName.indexOf('.') == -1)
-				mMatchingAlgorithm = (MatchingAlgorithm) (Class.forName(MatchingConfiguration.get(Property.DEFAULT_NAMESPACE) + matchingAlgorithmClassName).asSubclass(MatchingAlgorithm.class).getConstructor().newInstance());
+				mMatchingAlgorithm = (MatchingAlgorithm) (Class
+						.forName(MatchingConfiguration.get(Property.DEFAULT_NAMESPACE) + matchingAlgorithmClassName)
+						.asSubclass(MatchingAlgorithm.class).getConstructor().newInstance());
 			else
-				mMatchingAlgorithm = (MatchingAlgorithm) (Class.forName(matchingAlgorithmClassName).asSubclass(MatchingAlgorithm.class).getConstructor().newInstance());
+				mMatchingAlgorithm = (MatchingAlgorithm) (Class.forName(matchingAlgorithmClassName)
+						.asSubclass(MatchingAlgorithm.class).getConstructor().newInstance());
 		} catch (Exception e) {
 			LOGGER.error("Problem loading matching algorithm (" + matchingAlgorithmClassName + "): ", e);
 		}
 	}
 
 	@Override
-	public void close() throws IOException 
-	{
+	public void close() throws IOException {
 		mIndex.close();
 	}
 
 	abstract public ResultSet run(final SearchRequest srq) throws IOException;
-	
-	public final int TOP_K() 
-	{ 
-		return MatchingConfiguration.getInt(Property.TOP_K); 
-	}	
-	
+
+	public final int TOP_K() {
+		return MatchingConfiguration.getInt(Property.TOP_K);
+	}
+
 	abstract public void reset_to(final int to) throws IOException;
+
+
+	//below here is the Terrier-Core Matching implementation
+	
+	@Override
+	public String getInfo() {
+		return this.getClass().getSimpleName();
+	}
+
+	@Override
+	public org.terrier.matching.ResultSet match(String queryNumber, MatchingQueryTerms queryTerms) throws IOException {
+		LOGGER.info("Converting " + queryNumber + " from terrier to terrier-micro");
+		int queryId = Integer.parseInt(queryNumber.replaceAll("\\D+", ""));
+
+
+		//TODO this does not support query expansion or other forms of the query.
+		//TODO be clear on stemming
+		SearchRequest mSRQ = new SearchRequest(queryId, "");		
+		Query microQuery = mSRQ.getQuery();
+		for (MatchingTerm mt : queryTerms)
+		{
+			if (!( mt.getKey() instanceof SingleTermOp ))
+			{
+				throw new UnsupportedOperationException("QueryOp " + mt.getKey().getClass().getSimpleName() + " is not supported");
+			} 
+			SingleTermOp stq = (SingleTermOp) mt.getKey();
+			if (mt.getValue().weight != 1.0d)
+			{
+				LOGGER.warn("QueryOp with weight <> 1 are not supported");
+			}
+			microQuery.addTerm(stq.getTerm());
+		}
+		
+		ResultSet localRS = run(mSRQ);
+		int K = localRS.size();
+		QueryResultSet rtr = new QueryResultSet(K);
+		System.arraycopy(localRS.docids(), 0, rtr.getDocids(), 0, K);
+		System.arraycopy(toDoubleArray(localRS.scores()), 0, rtr.getScores(), 0, K);
+		LOGGER.info("terrier-micro finished with "+ queryNumber  + " - obtained " + K + " results in " + (processingTime/1000) + "ms");
+		return rtr;
+	}
+
+	@Override
+	public void setCollectionStatistics(CollectionStatistics cs) {
+		throw new UnsupportedOperationException();
+	}
+
+	double[] toDoubleArray(float[] arr) {
+		if (arr == null) return null;
+		int n = arr.length;
+		double[] ret = new double[n];
+		for (int i = 0; i < n; i++) {
+		  ret[i] = (double)arr[i];
+		}
+		return ret;
+	  }
+	
 }
+
